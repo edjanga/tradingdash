79a77b3c0dccd8fc162c8fd565cbebe2d8130bc3
edjanga <emmanuel.djanga@live.be>
2022-08-17 19:30:12 +0100

    Remove API key 3

--- src/tradingDashboard/__init__.py
+++ src/tradingDashboard/__init__.py
@@ -1,5 +1,5 @@
 from src.tradingDashboard.Backtest import Table, Performance
-from src.tradingDashboard.config import TiingoConfig
+#from src.tradingDashboard.config import TiingoConfig
 from src.tradingDashboard.data import Data
 from src.tradingDashboard.logger import Logs
 from src.tradingDashboard.Strategies import BuyAndHold,TacticalAllocation,PortfolioStrategies
